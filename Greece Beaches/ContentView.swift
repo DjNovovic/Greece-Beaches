034fed4ebcb0a1af4693e32c02937efa7fcbb77e
DjNovovic <djordjijenovovic@gmail.com>
2020-08-18 19:07:58 +0200

    Initial Commit

---
+++ Greece Beaches/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Greece Beaches
+//
+//  Created by Djordjije on 18/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Greece Beaches/Greece_BeachesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Greece_BeachesApp.swift
+//  Greece Beaches
+//
+//  Created by Djordjije on 18/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct Greece_BeachesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
